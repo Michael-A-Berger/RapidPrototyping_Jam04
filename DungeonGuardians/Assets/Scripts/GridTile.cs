935a6a04f420d4b2ca654bef84cbb8e78ededd11
Michael Berger (RIT Student) <mxb3145@ad.rit.edu>
2020-02-15 15:31:43 -0500

    Created GridTile script + Added helper methods to GridManager script

---
+++ DungeonGuardians/Assets/Scripts/GridTile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+    // Public Properties
+    public GameObject placedTower = null;
+    public Vector2Int gridPos = new Vector2Int(int.MinValue, int.MinValue);
+    public bool canPlaceTowers = true;
+}
