b11f0a7a9166b425bfb4028b0cd71c7401eb5137
Surya <suryan993@gmail.com>
2020-02-15 17:25:18 -0500

    added base class for towers

---
+++ DungeonGuardians/Assets/Scripts/ArrowTower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTower : Tower
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        type = TOWERTYPE.Arrow;
+        damage = 1;
+        maxTargets = 1;
+        range = 3;
+        attackSpeed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DungeonGuardians/Assets/Scripts/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Base class for all types of towers
+public abstract class Tower : MonoBehaviour
+{
+    public enum TOWERTYPE{Arrow,Heal,AOE};
+    protected TOWERTYPE type;
+    protected int range;
+    protected int damage;
+    protected int maxTargets;
+    protected int attackSpeed;
+    protected GridTile position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void placeTower(GridTile tile)
+    {
+        this.position = tile;
+    }
+}
