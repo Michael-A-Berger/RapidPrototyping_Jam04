c430ae568a26c8f628b148168ac303e030830acc
Michael Berger (RIT Student) <mxb3145@ad.rit.edu>
2020-02-15 15:53:57 -0500

    Started player cursor functionality

---
+++ DungeonGuardians/Assets/Scripts/PlayerCursor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCursor : MonoBehaviour
+{
+    // Public Properties
+    public GridManager gridManager = null;
+    public int playerNum = 1;
+    public bool usingController = false;
+
+    // Start()
+    void Start()
+    {
+        // IF the grid manager is null...
+        if (gridManager == null)
+        {
+            // Finding the grid manager
+            GameObject holder = GameObject.Find("GridHolder");
+            if (holder != null)
+                gridManager = holder.GetComponent<GridManager>();
+            else
+                Debug.LogError("\"gridManager\" was not set and could not be set dynamically!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
