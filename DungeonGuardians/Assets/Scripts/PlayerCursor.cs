8ddabe6228867476c78f748d2c86dbd4bc020fec
Michael Berger (RIT Student) <mxb3145@ad.rit.edu>
2020-02-15 16:54:47 -0500

    More work on Player tile selection

--- DungeonGuardians/Assets/Scripts/PlayerCursor.cs
+++ DungeonGuardians/Assets/Scripts/PlayerCursor.cs
@@ -6,22 +6,66 @@ public class PlayerCursor : MonoBehaviour
 {
     // Public Properties
     public GridManager gridManager = null;
+    [Range(1,4)]
     public int playerNum = 1;
     public bool usingController = false;
+
+    // Private Properties
+    private GameObject cursor = null;
 
     // Start()
     void Start()
     {
-        // IF the grid manager is null...
+        // IF the grid manager is null, complain about it
         if (gridManager == null)
         {
-            // Finding the grid manager
-            GameObject holder = GameObject.Find("GridHolder");
-            if (holder != null)
-                gridManager = holder.GetComponent<GridManager>();
-            else
-                Debug.LogError("\"gridManager\" was not set and could not be set dynamically!");
+            Debug.LogError("\"gridManager\" was not set!");
         }
+
+        // Getting the child cursor object
+        cursor = transform.Find("Cursor").gameObject;
+
+        // Starting the wait coroutine so that the GridManager can create the grid
+        StartCoroutine(WaitToStart());
+    }
+
+    // WaitToStart()
+    IEnumerator WaitToStart()
+    {
+        yield return new WaitForSeconds(0.01f);
+        SetCursorStart();
+    }
+
+    // SetCursorStart()
+    public void SetCursorStart()
+    {
+        Vector2Int gridSize = gridManager.GetGridSize();
+        switch (playerNum)
+        {
+            case 1:
+                PlaceCursorAtPos(0, 0);
+                break;
+            case 2:
+                PlaceCursorAtPos(gridSize.x - 1, 0);
+                break;
+            case 3:
+                PlaceCursorAtPos(0, gridSize.y - 1);
+                break;
+            case 4:
+                PlaceCursorAtPos(gridSize.x - 1, gridSize.y - 1);
+                break;
+            default:
+                Debug.LogError("Case for Player "+playerNum+" in SetCursorStart() is not defined!");
+                break;
+        }
+    }
+
+    // PlaceCursorAtPos()
+    public void PlaceCursorAtPos(int x, int y)
+    {
+        GridTile tileScript = gridManager.GetGridTileAtPos(x, y);
+        cursor.transform.position = tileScript.gameObject.transform.position;
+        cursor.transform.Translate(0.0f, 0.0f, -1f);
     }
 
     // Update is called once per frame
